0eeec5c339914e3da0e409efff5f54e859d8e89a
Christian Cortese <christian.cortese.cc@gmail.com>
2024-09-23 08:58:50 +0200

    added some props and use state to change application workload

--- 02-starting-react-vite/src/components/PostsList.jsx
+++ 02-starting-react-vite/src/components/PostsList.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+import NewPost from './NewPost';
+import Modal from './Modal';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
-    return <ul className={classes.posts}>
-        <Post author="Christian" body="React is perfect" />
-        <Post author="Ugo" body="I prefer next.js" />
-    </ul>
+    const [modalVisible, setModalVisible] = useState(true);
+    const [enteredBody, setEnteredBody ] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function hideModalHandler() {
+        setModalVisible(false);
+    }
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    return (
+        <>  
+            {modalVisible ? (
+                <Modal onClose={hideModalHandler}>
+            
+                <NewPost 
+                onBodyChange={bodyChangeHandler} 
+                onAuthorChange={changeAuthorHandler}
+                />
+            </Modal>) : false }
+
+            <ul className={classes.posts}>
+                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author="Ugo" body="I prefer next.js" />
+            </ul>
+        </>
+    );
 }
 
 export default PostsList;
